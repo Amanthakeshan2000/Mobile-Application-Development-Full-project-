6f7fafaac3542a29d811b7f1809703614369d62b
sevindunimrod <98372381+sevindunimrod@users.noreply.github.com>
2023-08-30 00:38:06 +0530

    add

---
+++ mobile_application/lib/pages/personal_details_1.dart
@@ -0,0 +1,14 @@
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
