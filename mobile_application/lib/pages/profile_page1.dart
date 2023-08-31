6dcddaafaa1009d5cc6cc1189aa5be68f89af753
kaveesha824 <kaveesha365@gmail.com>
2023-08-31 23:11:03 +0530

    add

--- mobile_application/lib/pages/profile_page1.dart
+++ mobile_application/lib/pages/profile_page1.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Center(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              _buildProfileInfo(),
+              const SizedBox(height: 20),
               _buildButton("Edit profile"),
               _buildButton("Verify account"),
               _buildButton("My courses"),
@@ -26,29 +42,56 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 
+  Widget _buildProfileInfo() {
+    const String userName = "John Doe";
+    const String profileImageUrl = "your_profile_image_url_here";
+
+    return const Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundImage: NetworkImage(profileImageUrl),
+          radius: 30,
+        ),
+        SizedBox(width: 10),
+        Text(
+          userName,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget _buildButton(String buttonText) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: ElevatedButton(
         onPressed: () {},
         style: ElevatedButton.styleFrom(
-          backgroundColor: Color.fromARGB(255, 235, 229, 229),
+          backgroundColor: const Color.fromARGB(255, 235, 229, 229),
           alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16), // Adjust padding here
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               buttonText,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.black,
               ),
             ),
-            Icon(Icons.arrow_forward_ios,
-                color: const Color.fromARGB(255, 155, 150, 150)),
+            const Icon(Icons.arrow_forward_ios,
+                color: Color.fromARGB(255, 155, 150, 150)),
           ],
         ),
       ),
     );
   }
 }
+
+
