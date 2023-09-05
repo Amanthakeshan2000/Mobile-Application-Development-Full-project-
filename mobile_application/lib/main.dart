7dd1b35e8701c14b8332d465daf979d926230b4b
LasithPerera <lasith.chamuditha7788@gmail.com>
2023-09-05 06:53:10 +0530

    main.dart update

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'reset_password_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Password Reset Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: PasswordResetPage(),
     );
   }
 }
+
