aa1a1dfbd8596bafe58a04ecf3eb236ff94caae7
sevindunimrod <98372381+sevindunimrod@users.noreply.github.com>
2023-09-05 20:50:52 +0530

    appbar

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -1,7 +1,18 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
 import 'pages/personal_details_1.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
 }
--- mobile_application/lib/pages/personal_details_1.dart
+++ mobile_application/lib/pages/personal_details_1.dart
@@ -1,16 +1,47 @@
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
-//change 1
+import 'package:flutter/material.dart';
 
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent, // Make the app bar transparent
+          elevation: 0, // Remove the shadow
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black, // Set arrow button color to black
+            ),
+            onPressed: () {
+              Navigator.pop(context); // Add back functionality
+            },
+          ),
+          title: Text(
+            "Personal Details",
+            style: TextStyle(
+              color: Colors.black, // Set text color to black
+              fontSize: 20.0,
+              fontFamily: 'Poppins', // Set the font family to Poppins
+              fontWeight: FontWeight.w600, // Set font weight to semi-bold
+            ),
+          ),
+          centerTitle: true, // Center align the title
         ),
-      ),
-    );
+        body: (Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 1, 14),
+            constraints: BoxConstraints(maxWidth: 320),
+            child: Text(
+              "Your personal information will helps our service to reach you easily",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16.0,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+                height: 1.5,
+              ),
+            ))));
   }
 }
