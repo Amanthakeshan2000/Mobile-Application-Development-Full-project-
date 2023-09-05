69a3974c34728ff8fcfbdc440048c38b6bb91375
hettiarachchisp526 <subodhapriyathushara01@gmail.com>
2023-09-05 13:58:46 +0530

    first change

---
+++ mobile_application/lib/pages/signup.dart
@@ -0,0 +1,60 @@
+
+// ignore_for_file: prefer_const_constructors, camel_case_types, prefer_const_literals_to_create_immutables, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class signUP extends StatelessWidget{
+  const signUP ({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+         appBar: AppBar(
+        title:  Center(),
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255).withOpacity(0), 
+        ),
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                      "Assets/signup.png",
+                       height: 243,
+                       width: 243,
+                       fit: BoxFit.cover,
+                    ),
+              ),
+                  
+                  Center(
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                     ),
+                  )
+               
+        
+             
+              
+            ],),
+        ),
+      ),
+      ),
+     
+      
+      
+      
+    );
+  }
+}
