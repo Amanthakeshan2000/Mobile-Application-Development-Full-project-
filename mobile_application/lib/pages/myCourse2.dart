20885e47d986c3e9ba05749f8f84a95fb9f8aae6
Bhagya-Senevirathna <bhagyasenevirathna@gmail.com>
2023-09-21 22:09:48 +0530

    addsixth

--- mobile_application/lib/pages/myCourse2.dart
+++ mobile_application/lib/pages/myCourse2.dart
@@ -10,6 +10,16 @@ class MyCourse2 extends StatefulWidget {
 class _MyCourse2State extends State<MyCourse2> {
   int? selectedAnswer;
 
+  // Create a list to track the expansion panel state
+  List<bool> isPanelExpanded = [false, false, false];
+
+  // Additional text content for each panel
+  List<String> additionalTextContent = [
+    'c. System.out.println("Hello World");',
+    'class HelloWorld { \npublic static void main(String[] args) { \nSystem. out. println("Hello World!"); \n} \n}',
+    'class HelloWorld :\nDeclares a class named "HelloWorld".\n\npublic static void main(String[] args) :\nDeclares the special entry point method called "main" that accepts an array of strings as command-line arguments.\n\nSystem.out.println("Hello World!"); :\nPrints the message "Hello World!" to the console using the println() method of the System.out object.',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,20 +82,20 @@ class _MyCourse2State extends State<MyCourse2> {
               ),
             ),
             // Add the additional containers with texts and dropdown icons
-            buildAdditionalContainer("Answer"),
-            buildAdditionalContainer("Sample Code"),
-            buildAdditionalContainer("Explanation"),
+            buildAdditionalContainer("Answer", 0),
+            buildAdditionalContainer("Sample Code", 1),
+            buildAdditionalContainer("Explanation", 2),
           ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0), // Add 8 pixels horizontal padding
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: const EdgeInsets.only(right: 5.0), // Add 5 pixels margin between buttons
+                margin: const EdgeInsets.only(right: 5.0),
                 child: ElevatedButton(
                   onPressed: () {
                     // Clear button action
@@ -94,17 +104,17 @@ class _MyCourse2State extends State<MyCourse2> {
                     });
                   },
                   style: ElevatedButton.styleFrom(
-                    primary: const Color.fromARGB(255, 165, 152, 151), // Customize button color
-                    minimumSize: const Size(120, 40), // Set button size
+                    primary: const Color.fromARGB(255, 165, 152, 151),
+                    minimumSize: const Size(120, 40),
                   ),
                   child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0), // Adjust horizontal padding
-                    child: Text("Clear", style: TextStyle(fontSize: 16.0)), // Adjust button text size
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Text("Clear", style: TextStyle(fontSize: 16.0)),
                   ),
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(left: 5.0), // Add 5 pixels margin between buttons
+                margin: const EdgeInsets.only(left: 5.0),
                 child: ElevatedButton(
                   onPressed: () {
                     // Submit button action
@@ -113,11 +123,11 @@ class _MyCourse2State extends State<MyCourse2> {
                     }
                   },
                   style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(120, 40), // Set button size
+                    minimumSize: const Size(120, 40),
                   ),
                   child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0), // Adjust horizontal padding
-                    child: Text("Submit", style: TextStyle(fontSize: 16.0)), // Adjust button text size
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Text("Submit", style: TextStyle(fontSize: 16.0)),
                   ),
                 ),
               ),
@@ -145,24 +155,44 @@ class _MyCourse2State extends State<MyCourse2> {
     );
   }
 
-  Widget buildAdditionalContainer(String labelText) {
+  Widget buildAdditionalContainer(String labelText, int index) {
     return Container(
       margin: const EdgeInsets.all(16.0),
       padding: const EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        color: Colors.grey[300], // Set the ash color here
-        borderRadius: BorderRadius.circular(12.0), // Set border radius
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12.0),
       ),
-      child: Row(
+      child: Column(
         children: [
-          Text(
-            labelText,
-            style: const TextStyle(
-              color: Colors.black,
+          ListTile(
+            title: Text(
+              labelText,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            trailing: IconButton(
+              icon: isPanelExpanded[index]
+                  ? const Icon(Icons.arrow_drop_up)
+                  : const Icon(Icons.arrow_drop_down),
+              onPressed: () {
+                setState(() {
+                  isPanelExpanded[index] = !isPanelExpanded[index];
+                });
+              },
             ),
           ),
-          const Spacer(),
-          const Icon(Icons.arrow_drop_down),
+          if (isPanelExpanded[index])
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                additionalTextContent[index],
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
         ],
       ),
     );
